f4cfb63be5202f8163401daff80cb9ce56645bcd
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-04-06 15:28:28 +0200

    update4

--- Err.c
+++ Err.c
@@ -1,104 +1,114 @@
 #pragma once
+#include "sl_funcs.c"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include "sl_funcs.c"
-#define MAX_ERROR_MSG_LENGTH 256
 
-typedef struct{
-    int code;
-    char message[MAX_ERROR_MSG_LENGTH];
+#define MAX_ERROR_MSG_LENGTH 0x100
+typedef struct {
+  int code;
+  char message[MAX_ERROR_MSG_LENGTH];
 } Error;
 typedef struct {
-int code;
-char message[MAX_ERROR_MSG_LENGTH];
-Rgb color;
-}defErr;
-defErr getDefErr(Rgb color,Error error) {
-char* defBuf[sizeof(error.message)];
-    sprintf(defBuf,"Error %d: %s\n", error.code, error.message);
-defErr def={
-    .code=error.code,
-    .color=color,
-    .message=error.message
-};
-return def;
+  int code;
+  char message[MAX_ERROR_MSG_LENGTH];
+  Rgb color;
+} defErr;
+defErr getDefErr(Rgb color, Error error) {
+  char *defBuf[sizeof(error.message)];
+  sprintf(defBuf, "Error %d: %s\n", error.code, error.message);
+  defErr def = {.code = error.code, .color = color, .message = error.message};
+  return def;
 }
 void default_error_handler(Error error) {
-char* defBuf[sizeof(error.message)];
-    sprintf(defBuf,"Error %d: %s\n", error.code, error.message);
-    cException(WHITE,defBuf);
-    exit(error.code);
+  char *defBuf[sizeof(error.message)];
+  sprintf(defBuf, "Error %d: %s\n", error.code, error.message);
+  cException(WHITE, defBuf);
+  exit(error.code);
 }
 typedef void (*ErrorHandler)(Error);
 
 void handle_error(ErrorHandler handler, Error error) {
-    if (handler != NULL) {
-        handler(error);
-    } else {
-        default_error_handler(error);
-    }
+  if (handler != NULL) {
+    handler(error);
+  } else {
+    default_error_handler(error);
+  }
 }
-void cHandleErr(Rgb color,ErrorHandler handler, Error error) {
-    if (handler != NULL) {
-        handler(error);
-    } else {
-        defErr err=getDefErr(color,error);
-        cException(err.color,err.message);
-        exit(err.code);
-    }
+void cHandleErr(Rgb color, ErrorHandler handler, Error error) {
+  if (handler != NULL) {
+    handler(error);
+  } else {
+    defErr err = getDefErr(color, error);
+    cException(err.color, err.message);
+    exit(err.code);
+  }
 }
 
 #define MAKE_ERROR(code, message) make_error(code, message, __FILE__, __LINE__)
 
 Error make_error(int code, const char *message, const char *file, int line) {
-    Error error;
-    error.code = code;
-    snprintf(error.message, MAX_ERROR_MSG_LENGTH, "%s (%s:%d)", message, file, line);
-    error.message[MAX_ERROR_MSG_LENGTH - 1] = '\0';
-    return error;
+  Error error;
+  error.code = code;
+  snprintf(error.message, MAX_ERROR_MSG_LENGTH, "%s (%s:%d)", message, file,
+           line);
+  error.message[MAX_ERROR_MSG_LENGTH - 1] = '\0';
+  return error;
 }
 
-#define CHECK_NULL(pointer) do { \
-    if ((pointer) == NULL) { \
-        handle_error(NULL, MAKE_ERROR(1, "Null pointer")); \
-    } \
-} while (0)
+#define CHECK_NULL(pointer)                                                    \
+  do {                                                                         \
+    if ((pointer) == NULL) {                                                   \
+      handle_error(NULL, MAKE_ERROR(1, "Null pointer"));                       \
+    }                                                                          \
+  } while (0)
 
-#define CHECK_FILE(file, mode) do { \
-    if ((file) == NULL) { \
-        handle_error(NULL, MAKE_ERROR(errno, strerror(errno))); \
-    } \
-} while (0)
+#define CHECK_FILE(file, mode)                                                 \
+  do {                                                                         \
+    if ((file) == NULL) {                                                      \
+      handle_error(NULL, MAKE_ERROR(errno, strerror(errno)));                  \
+    }                                                                          \
+  } while (0)
 
-#define CHECK(condition, code, message) do { \
-    if ((condition)) { \
-        handle_error(NULL, MAKE_ERROR(code, message)); \
-    } \
-} while (0)
+#define CHECK(condition, code, message)                                        \
+  do {                                                                         \
+    if ((condition)) {                                                         \
+      handle_error(NULL, MAKE_ERROR(code, message));                           \
+    }                                                                          \
+  } while (0)
 
-#define CHECK_MSG(condition, message) do { \
-    if ((condition)) { \
-        handle_error(NULL, MAKE_ERROR(1, message)); \
-    } \
-} while (0)
-#define CCHECK_MSG(color,condition, message) do { \
-    if ((condition)) { \
-        cHandleErr(color,NULL,MAKE_ERROR(1, message));\
-    } \
-} while (0)
+#define CHECK_MSG(condition, message)                                          \
+  do {                                                                         \
+    if ((condition)) {                                                         \
+      handle_error(NULL, MAKE_ERROR(1, message));                              \
+    }                                                                          \
+  } while (0)
+#define CCHECK_MSG(color, condition, message)                                  \
+  do {                                                                         \
+    if ((condition)) {                                                         \
+      cHandleErr(color, NULL, MAKE_ERROR(1, message));                         \
+    }                                                                          \
+  } while (0)
 
-#define TRY(func) do { \
-    Error __error__; \
-    if ((__error__ = func).code != 0) { \
-        handle_error(NULL, __error__); \
-    } \
-} while (0)
+#define CHECK_BODY(condition, body)                                            \
+  do {                                                                         \
+    if ((condition)) {                                                         \
+      body;                                                                    \
+    }                                                                          \
+  } while (0)
+#define TRY(func)                                                              \
+  do {                                                                         \
+    Error __error__;                                                           \
+    if ((__error__ = func).code != 0) {                                        \
+      handle_error(NULL, __error__);                                           \
+    }                                                                          \
+  } while (0)
 
-#define TRY_WITH_HANDLER(func, handler) do { \
-    Error __error__; \
-    if ((__errr__ = func).code != 0) { \
-        handle_error(handler, __error__); \
-    } \
-} while (0)
+#define TRY_WITH_HANDLER(func, handler)                                        \
+  do {                                                                         \
+    Error __error__;                                                           \
+    if ((__errr__ = func).code != 0) {                                         \
+      handle_error(handler, __error__);                                        \
+    }                                                                          \
+  } while (0)
--- print.c
+++ print.c
@@ -1,33 +1,34 @@
 #pragma once
-#include <stdio.h>
 #include "Err.c"
 #include <stdarg.h>
+#include <stdio.h>
+
 #define MAX_FORMAT_SIZE 256
-#define MAX_STRING_SIZE 1024
+#define MAX_STRING_SIZE 0xFF * 3
 
 void print(const char *format, ...) {
-    char format_buffer[MAX_FORMAT_SIZE];
-    char string_buffer[MAX_STRING_SIZE];
+  char format_buffer[MAX_FORMAT_SIZE];
+  char string_buffer[MAX_STRING_SIZE];
 
-    int format_index = 0, i = 0;
-    while (format[i] != '\0' && format_index < MAX_FORMAT_SIZE - 2) {
-        if (format[i] == '{' && format[i + 1] == '}') {
-            format_buffer[format_index++] = '%';
-            format_buffer[format_index++] = 's';
-            i += 2;
-        } else {
-            format_buffer[format_index++] = format[i++];
-        }
+  int format_index = 0, i = 0;
+  while (format[i] != '\0' && format_index < MAX_FORMAT_SIZE - 2) {
+    if (format[i] == '{' && format[i + 1] == '}') {
+      format_buffer[format_index++] = '%';
+      format_buffer[format_index++] = 's';
+      i += 2;
+    } else {
+      format_buffer[format_index++] = format[i++];
     }
-    format_buffer[format_index] = '\0';
+  }
+  format_buffer[format_index] = '\0';
 
-    va_list args;
-    va_start(args, format);
-    int string_length = vsnprintf(string_buffer, MAX_STRING_SIZE, format_buffer, args);
-    va_end(args);
-    CHECK(string_length < 0,1,"vsnprintf error");
-    CHECK(string_length >= MAX_STRING_SIZE,1, "Error: string too long\n");
-    if (fwrite(string_buffer, string_length, 1, stdout) != 1) {
-        perror("fwrite error");
-    }
+  va_list args;
+  va_start(args, format);
+  int string_length =
+      vsnprintf(string_buffer, MAX_STRING_SIZE, format_buffer, args);
+  va_end(args);
+  CHECK(string_length < 0, 1, "vsnprintf error");
+  CHECK(string_length >= MAX_STRING_SIZE, 1, "Error: string too long\n");
+  CHECK_MSG(fwrite(string_buffer, string_length, 1, stdout) != 1,
+            "fwrite error");
 }
