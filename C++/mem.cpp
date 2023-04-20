b915c4c893544fbaade7aab9c4d704179fef1fad
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-04-20 07:29:56 +0200

    update alpha 2.6.4

--- C++/mem.cpp
+++ C++/mem.cpp
@@ -1,105 +1,93 @@
 #pragma once
-#include <cstring>
-#include <cstddef>
-#include <memory>
-constexpr size_t Gigabytes(size_t gb) { return gb * 1024 * 1024 * 1024; }
-constexpr size_t Kilobytes(size_t kb) { return kb * 1024; }
-class MemoryAllocator {
-public:
-    MemoryAllocator() = default;
+#define DEF_MATH
+#define DEF
+#define NORMAL
+#include "core.hpp"
+#include "hash.cpp"
 
-    void* Allocate(size_t size) {
-        void* ptr = heap_ + size;
-        heap_size_ += size;
-        return ptr;
-    }
+static std::array<i8, Gigabytes(1)> heap;
+static std::array<i8, Gigabytes(1)> chunker;
+size_t chunkerSize, heapSize;
+typedef struct {
+  void *ptr;
+  size_t sz;
+} Chunk;
+Chunk chunkAlloc(size_t sz) {
+  void *chunk = chunker + sz;
+  chunkerSize += sz;
+  Chunk newChunk = {.ptr = chunk, .sz = sz};
+  return newChunk;
+}
+void *hAlloc(size_t sz) {
+  void *h = heap + sz;
+  heapSize += sz;
+  return h;
+}
 
-    void* AllocateBox(size_t size) {
-        void* ptr = heap_ + size;
-        heap_size_ += size;
-        return new Box{ ptr, size };
-    }
+Chunk hAllocChunk(size_t sz) {
+  void *h = heap + sz;
+  heapSize += sz;
+  Chunk newBox = {h,sz};
+  return newBox;
+}
+void passHeap() {
+  for (i32 i = 0; i < chunkerSize; ++i) {
+    heap[i] = chunker[i];
+  }
+  heapSize = chunkerSize;
+  chunkerSize = null;
+}
 
-    void Free(void* ptr, size_t size) {
-        std::memset(ptr, 0, size);
-        heap_size_ -= size;
-    }
+void bringDown() {
+  for (i32 i = 0; i < chunkerSize; ++i) {
+    chunker[i] = null;
+  }
+}
+void hFree(void *ptr, size_t sz) {
+  memset(ptr, 0, sz);
+  heapSize -= sz;
+}
+void *hReAlloc(void *ptr, size_t oldSz, size_t newSz) {
+  if (newSz <= oldSz) {
+    return ptr;
+  }
 
-    void* Reallocate(void* ptr, size_t old_size, size_t new_size) {
-        if (new_size <= old_size) {
-            return ptr;
+  void *newPtr = hAlloc(newSz);
+  memcpy(newPtr, ptr, oldSz);
+  hFree(ptr, oldSz);
+
+  return newPtr;
+}
+void *moveToChunk(void *ptr) {
+  for (size_t i = 0; i < heapSize; ++i) {
+    if (heap + i == ptr) {
+      size_t sz = 0;
+      // Find the size of the chunk to move
+      for (size_t j = i; j < heapSize; ++j) {
+        if (heap[j] == 0) {
+          sz = j - i;
+          break;
         }
+      }
 
-        void* new_ptr = Allocate(new_size);
-        std::memcpy(new_ptr, ptr, old_size);
-        Free(ptr, old_size);
+      if (sz == 0) {
+        // The chunk extends to the end of the heap
+        sz = heapSize - i;
+      }
 
-        return new_ptr;
+      // Move the chunk to the chunker
+      Chunk newChunk = chunkAlloc(sz);
+      memcpy(newChunk.ptr, ptr, sz);
+
+      // Clear the original chunk in the heap
+      memset(ptr, 0, sz);
+
+      // Update heapSize
+      heapSize -= sz;
+
+      return newChunk.ptr;
     }
+  }
 
-    void* MoveToChunk(void* ptr) {
-        for (size_t i = 0; i < heap_size_; ++i) {
-            if (heap_ + i == ptr) {
-                size_t size = 0;
-
-                // Find the size of the chunk to move
-                for (size_t j = i; j < heap_size_; ++j) {
-                    if (heap_[j] == 0) {
-                        size = j - i;
-                        break;
-                    }
-                }
-
-                if (size == 0) {
-                    // The chunk extends to the end of the heap
-                    size = heap_size_ - i;
-                }
-
-                // Move the chunk to the chunker
-                Chunk* new_chunk = new Chunk{ chunker_ + size, size };
-                std::memcpy(new_chunk->ptr, ptr, size);
-
-                // Clear the original chunk in the heap
-                std::memset(ptr, 0, size);
-
-                // Update heap_size_
-                heap_size_ -= size;
-
-                return new_chunk->ptr;
-            }
-        }
-
-        return nullptr;
-    }
-
-    void PassHeap() {
-        std::memcpy(heap_, chunker_, chunker_size_);
-        heap_size_ = chunker_size_;
-        chunker_size_ = 0;
-    }
-
-    void BringDown() {
-        std::memset(chunker_, 0, chunker_size_);
-    }
-
-private:
-    char heap_[Gigabytes(1)]{};
-    char chunker_[Kilobytes(7)]{};
-    size_t chunker_size_{};
-    size_t heap_size_{};
-
-    struct Chunk {
-        void* ptr{};
-        size_t size{};
-    };
-
-    struct Box {
-        void* ptr{};
-        size_t size{};
-
-        ~Box() {
-            std::memset(ptr, 0, size);
-            delete ptr;
-        }
-    };
-};
+  return nullptr;
+}
