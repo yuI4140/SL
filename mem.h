9cc62660a4bb04b69db922654ae6af7b6fe08992
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-08-16 11:45:09 +0200

    Update mem.h

--- mem.h
+++ mem.h
@@ -1,52 +1,60 @@
 #include <stdlib.h>
+#include <stdio.h> 
+
 typedef struct {
-  size_t cap;
-  size_t size;
-  void* db; 
+    size_t cap;
+    size_t size;
+    void* db;
 } Ren;
-void rAssert(int condition); 
-void rpAssert(void* condition); 
-void rAssert(int condition); 
-Ren* createRen(size_t cap); 
-void destroyRen(Ren* ren); 
-void* allocRen(Ren* ren, size_t size); 
-void dropDown(Ren* ren); 
+
+void rAssert(int condition, const char* message); // Added error message parameter
+Ren* createRen(size_t cap);
+void destroyRen(Ren* ren);
+void* allocRen(Ren* ren, size_t size);
+void dropDown(Ren* ren);
+
 #ifdef MEM_IMP
-void rAssert(int condition) {
-  if (!condition) {
-    exit(1);
-  }
+void rAssert(int condition, const char* message) {
+    if (!condition) {
+        fprintf(stderr, "Error: %s\n", message);
+        exit(1);
+    }
 }
-void rpAssert(void* condition){
-  if (!condition) {
-    exit(1);
-  }
-}
+
 Ren* createRen(size_t cap) {
-  Ren* ren = (Ren*)malloc(sizeof(Ren));
-  ren->cap = cap;
-  ren->size = 0;
-  ren->db = malloc(cap);
-  return ren;
+    Ren* ren = (Ren*)malloc(sizeof(Ren));
+    rAssert(ren, "Memory allocation failed for Ren");
+    
+    ren->cap = cap;
+    ren->size = 0;
+    ren->db = malloc(cap);
+    if (!ren->db) {
+        free(ren);
+        ren = NULL; 
+        rAssert(0, "Memory allocation failed for Ren database");
+    }
+    return ren;
 }
 
 void destroyRen(Ren* ren) {
-  if (ren) {
-    free(ren->db);
-    free(ren);
-  }
+    if (ren) {
+        free(ren->db);
+        free(ren);
+    }
 }
 
 void* allocRen(Ren* ren, size_t size) {
-  rAssert(!ren || size == 0);
-  rAssert(ren->size + size > ren->cap);
-  void* mem = ren->db + ren->size; 
-  ren->size += size;
-  return mem;
+    rAssert(ren && size > 0, "Invalid arguments in allocRen");
+    rAssert(ren->size + size <= ren->cap, "Memory allocation would exceed capacity");
+    
+    void* mem = (char*)ren->db + ren->size;
+    ren->size += size;
+    return mem;
 }
 
 void dropDown(Ren* ren) {
-  if (ren)
-    ren->size = 0;
+    if (ren) {
+        ren->size = 0;
+    }
 }
 #endif // MEM_IMP
